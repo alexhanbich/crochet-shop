8f097853686848414bf78c817b5fdb28b39a0baa
Alex Kang <63813437+alexhanbich@users.noreply.github.com>
2023-10-12 20:31:08 -0600

    fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,18 +5,7 @@ import { ToastContainer, Flip } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Footer from "./components/Footer";
 
-import { Elements } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
-import CheckoutForm from "./components/CheckoutForm";
-
-const stripePromise = loadStripe(
-  "pk_test_51O0ae6A654jd0kXKIAiOhx4IKBc5Iwabd9zsFHukkdlFzJIPYKvqmjx37d9PXymtagAc4gmQFrYJcRcoOkQtuHmy00aVaBw5V7"
-);
-
 const App = () => {
-  const options = {
-    clientSecret: "{{CLIENT_SECRET}}",
-  };
   return (
     <>
       <div className="max-w-screen-lg mx-auto font-body">
@@ -30,9 +19,6 @@ const App = () => {
           style={{ width: "200px" }}
         />
         <Footer />
-        <Elements stripe={stripePromise} options={options}>
-          <CheckoutForm />
-        </Elements>
       </div>
     </>
   );
